c60d5228db5c0a8672c5db6869ff48b17c114b24
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2020-04-14 15:51:51 -0400

    change submit to button

--- src/sessions/Signup.js
+++ src/sessions/Signup.js
@@ -24,7 +24,7 @@ export default class Signup extends Component {
             <input type="radio" id="south" name="hemisphere" value="South" />
           </div>
         </div>
-        <input type="Submit" value="Sign up" />
+        <button>Sign Up</button>
       </form>
     );
   }
