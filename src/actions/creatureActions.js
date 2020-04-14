edde76bffa8197481c0bdeed4719ac7e7ed6855b
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2020-04-14 19:19:54 -0400

    move fetch logic to creature adapter

--- src/actions/creatureActions.js
+++ src/actions/creatureActions.js
@@ -1,13 +1,10 @@
+import creatureAdapter from '../adapters/creatureAdapter';
 const BASE_URL = "http://127.0.0.1:3001";
+
 export const fetchCurrentCreatures = () => {
   return dispatch => {
     dispatch({ type: "LOADING_CREATURES" });
-
-    fetch(`${BASE_URL}/current`)
-      .then(res => res.json())
-      .then(data => {
-        dispatch({ type: "ADD_CREATURES", data })
-      })
-      .catch(error => console.error)
+    console.log(dispatch)
+    creatureAdapter.fetchCurrentCreatures(dispatch)
   };
 }
