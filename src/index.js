63c8bb8bc16646dac214a522ec4e9d5e58edf6f3
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2020-04-14 14:20:34 -0400

    set up redux store

--- src/index.js
+++ src/index.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+
 import './index.css';
 import App from './App';
 
+import combinedReducers from './reducers/index.js';
+
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(combinedReducers, composeEnhancer(applyMiddleware(thunk)));
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
 
