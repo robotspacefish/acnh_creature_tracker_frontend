6c3bf34455df8e3af328aaf1522b21e0cbada961
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2020-04-14 15:38:20 -0400

    signup form layout

---
+++ src/sessions/Signup.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import './sessions.css';
+
+export default class Signup extends Component {
+  render() {
+    return (
+      <form className="Signup">
+        <div className="input-container">
+          <label>Username</label><br />
+          <input type="text" name="name" />
+        </div>
+        <div className="input-container">
+          <label>Password</label>
+          <input type="password" name="password" />
+        </div>
+        <div className="input-container">
+          <p>Select Your Hemisphere:</p>
+          <div className="input-container">
+            <label>North</label>
+            <input type="radio" id="north" name="hemisphere" value="North" />
+          </div>
+          <div className="input-container">
+            <label>South</label>
+            <input type="radio" id="south" name="hemisphere" value="South" />
+          </div>
+        </div>
+        <input type="Submit" value="Sign up" />
+      </form>
+    );
+  }
+}
