4d028ebc97cc8b8a9be5c65a761edf5cb42d5f23
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2020-04-14 14:44:30 -0400

    set up bare bones nav bar

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import NavBar from '../NavBar/NavBar';
 import './Header.css';
 
 const Header = () => (
   <div className="Header">
     <h1 className="title">ACNH Creature Tracker</h1>
+    <NavBar />
   </div>
 );
 
---
+++ src/NavBar/NavBar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './NavBar.css';
+
+const NavBar = props => (
+  <div className="Nav">
+    Login | Signup
+    {/* todo React Router
+    if !logged_in Login | Signup
+    if logged_in My Creatures */}
+  </div>
+);
+
+export default NavBar;
