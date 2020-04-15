63e8f6d91e4021d7ae923519041f406e9ce29171
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2020-04-14 22:57:34 -0400

    NavLinks for login, signup, displaying current user's creatures,& logout

--- src/NavBar/NavBar.js
+++ src/NavBar/NavBar.js
@@ -1,12 +1,23 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import './NavBar.css';
 
 const NavBar = props => (
   <div className="Nav">
-    Login | Signup
-    {/* todo React Router
-    if !logged_in Login | Signup
-    if logged_in My Creatures */}
+    <NavLink exact to='/'>Home</NavLink>
+    {!props.username ?
+      (<>
+        <NavLink exact to='/login'>Log In</NavLink>
+        <NavLink exact to='/signup'>Sign Up</NavLink>
+      </>) :
+      (
+        <>
+          Welcome, {props.username}!
+          <NavLink exact to={`/${props.username}/creatures`}>My Creatures</NavLink>
+          <NavLink exact to='/logout'>Log Out</NavLink>
+        </>
+      )
+    }
   </div>
 );
 
