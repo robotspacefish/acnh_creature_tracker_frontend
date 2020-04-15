117dab8e88933fbaf5da8823bec173909d19a6d8
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2020-04-15 09:02:50 -0400

    add LoadSpinner & list todos in component for now

---
+++ src/Home/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import LoadSpinner from '../LoadSpinner/LoadSpinner';
+
+const Home = props => {
+  return (
+    <div className="Home">
+      Todo:<br />
+      Time display (updates in real time)<br />
+      CreaturesContainer to display currently available creatures
+    </div>
+  );
+};
+
+export default Home;
