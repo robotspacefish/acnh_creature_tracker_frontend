f5caf82a7aecd1ac93ee47dfe0cbe2714ff1b7f4
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2020-04-14 22:58:06 -0400

    wrap App in Router

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware, compose } from 'redux';
+import { BrowserRouter as Router } from 'react-router-dom';
 import thunk from 'redux-thunk';
 
 import './index.css';
@@ -16,7 +17,9 @@ const store = createStore(combinedReducers, composeEnhancer(applyMiddleware(thun
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <Router>
+      <App />
+    </Router>
   </Provider>,
   document.getElementById('root')
 );
