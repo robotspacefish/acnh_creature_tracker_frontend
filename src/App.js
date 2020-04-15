aebd09f87e6c4a12b472a493094465acc01a18e2
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2020-04-14 22:11:57 -0400

    import Login & check for current user upon mounting

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import React, { Component } from 'react';
 import Header from './Header/Header';
 import Signup from './sessions/Signup';
+import Login from './sessions/Login';
 import LoadSpinner from './LoadSpinner/LoadSpinner';
 
 import './App.css';
 
 import { connect } from 'react-redux';
 import { fetchCurrentCreatures } from './actions/creatureActions';
+import { getCurrentUser } from './actions/userActions';
 
 class App extends Component {
   componentDidMount() {
-    this.props.fetchCurrentCreatures();
+    // this.props.fetchCurrentCreatures();
+    // this.props.getCurrentUser();
   }
 
   render() {
@@ -18,6 +21,7 @@ class App extends Component {
       <div className="App">
         <Header />
         {/* <Signup /> */}
+        <Login />
         {this.props.loadingCreatures && <LoadSpinner />}
       </div>
     );
@@ -33,7 +37,8 @@ const mapStateToProps = state => {
 
 const mapDispatchToProps = dispatch => {
   return {
-    fetchCurrentCreatures: () => dispatch(fetchCurrentCreatures())
+    fetchCurrentCreatures: () => dispatch(fetchCurrentCreatures()),
+    getCurrentUser: () => dispatch(getCurrentUser())
   }
 };
 
