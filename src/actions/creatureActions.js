5715d7b6bab9a3607b208eed3e7700f61beb8f5f
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2020-04-14 22:09:59 -0400

    move BASE_URL to utils

--- src/actions/creatureActions.js
+++ src/actions/creatureActions.js
@@ -1,10 +1,8 @@
 import CreatureAdapter from '../adapters/creatureAdapter';
-const BASE_URL = "http://127.0.0.1:3001";
 
 export const fetchCurrentCreatures = () => {
   return dispatch => {
     dispatch({ type: "LOADING_CREATURES" });
-    console.log(dispatch)
     CreatureAdapter.fetchCurrentCreatures(dispatch)
   };
 }
--- src/adapters/creatureAdapter.js
+++ src/adapters/creatureAdapter.js
@@ -1,4 +1,5 @@
-const BASE_URL = "http://127.0.0.1:3001/";
+import { BASE_URL } from '../helpers/utils';
+
 export default class CreatureAdapter {
   static fetchCurrentCreatures(dispatch) {
     fetch(`${BASE_URL}/current`)
