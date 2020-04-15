1a46e9704d22346768686492d825ac9cc748d4f1
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2020-04-15 10:58:12 -0400

    clear current user

--- src/reducers/userReducer.js
+++ src/reducers/userReducer.js
@@ -2,6 +2,8 @@ export default (state = { username: '' }, action) => {
   switch (action.type) {
     case "SET_CURRENT_USER":
       return action.user;
+    case "CLEAR_CURRENT_USER":
+      return { username: "" }
     default:
       return state;
   }
