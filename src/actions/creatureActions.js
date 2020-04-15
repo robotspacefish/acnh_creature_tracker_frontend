30bcd6f03c150650f0982c1046446b425f3620db
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2020-04-14 22:30:33 -0400

    fetchAllCreatures  & refactor fetch current

--- src/actions/creatureActions.js
+++ src/actions/creatureActions.js
@@ -3,6 +3,19 @@ import CreatureAdapter from '../adapters/creatureAdapter';
 export const fetchCurrentCreatures = () => {
   return dispatch => {
     dispatch({ type: "LOADING_CREATURES" });
-    CreatureAdapter.fetchCurrentCreatures(dispatch)
+
+    CreatureAdapter.fetchCreatures(dispatch, "current", addCurrentCreatures)
   };
 }
+
+export const fetchAllCreatures = () => {
+  return dispatch => {
+    dispatch({ type: "LOADING_CREATURES" });
+
+    CreatureAdapter.fetchCreatures(dispatch, "creatures", addAllCreatures)
+  }
+}
+
+const addCurrentCreatures = data => ({ type: "ADD_CURRENT_CREATURES", data });
+
+const addAllCreatures = data => ({ type: "ADD_ALL_CREATURES", data });
