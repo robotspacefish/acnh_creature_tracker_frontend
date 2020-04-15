2f16880fc2a83b3eb12db5ece8f3f0250d862ecf
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2020-04-15 16:38:12 -0400

    hemisphere select and type select components for creature display

---
+++ src/Creatures/CreaturesHemisphereSelect.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+export default class CreaturesHemisphereSelect extends Component {
+  render() {
+    return (
+      <div className="CreaturesHemisphereSelect">
+        <button>Northern Hemisphere</button>
+        <button>Southern Hemisphere</button>
+      </div>
+    );
+  }
+}
+
+
+
---
+++ src/Creatures/CreaturesTypeSelect.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+export default class CreaturesTypeSelect extends Component {
+  render() {
+    return (
+      <div className="CreaturesTypeSelect">
+        <button>All</button>
+        <button>Bugs</button>
+        <button>Fish</button>
+      </div>
+    );
+  }
+}
+
