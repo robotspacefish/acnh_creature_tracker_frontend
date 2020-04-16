6a7a2403988cdc05874e45ede2ced087e07c45a5
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2020-04-16 13:33:09 -0400

    handle on click to switch between creature types to display

--- src/Creatures/CreaturesTypeSelect.js
+++ src/Creatures/CreaturesTypeSelect.js
@@ -1,14 +1,20 @@
 import React, { Component } from 'react';
 
-export default class CreaturesTypeSelect extends Component {
+class CreaturesTypeSelect extends Component {
+  setActiveClass = type => (this.props.displayType === type ? 'active' : '');
+
+  handleOnClick = e => (this.props.setDisplayType(e.target.dataset.type));
+
   render() {
     return (
       <div className="CreaturesTypeSelect">
-        <button>All</button>
-        <button>Bugs</button>
-        <button>Fish</button>
+        <button data-type="all" className={this.setActiveClass("all")} onClick={this.handleOnClick}>All</button>
+        <button data-type="bugs" className={this.setActiveClass("bugs")} onClick={this.handleOnClick}>Bugs</button>
+        <button data-type="fish" className={this.setActiveClass("fish")} onClick={this.handleOnClick}>Fish</button>
       </div>
     );
   }
 }
+
+export default CreaturesTypeSelect;
 
