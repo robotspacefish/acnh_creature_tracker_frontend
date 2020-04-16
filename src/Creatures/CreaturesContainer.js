d7c93fb5841793e47bc3fd4febbed37fa9add5dc
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2020-04-16 12:03:46 -0400

    use connect & map creatures to props, pass to CreatureList

--- src/Creatures/CreaturesContainer.js
+++ src/Creatures/CreaturesContainer.js
@@ -1,14 +1,43 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux'
 import CreatureList from './CreatureList';
+import CreatureListHeader from './CreatureListHeader';
+import { sortByName } from '../actions/creatureActions';
+
 import './Creatures.css';
 
-const CreaturesContainer = props => {
+class CreaturesContainer extends Component {
+  state = {
+    displayType: 'all', // all, bugs, or fish
+    displayHemisphere: 'north'
+  }
 
-  return (
-    <div className="CreaturesContainer">
-      <CreatureList creatures={props.creatures} />
-    </div>
-  );
+  render() {
+    const { displayType, displayHemisphere } = this.state;
+    const creatures = this.props[this.props.creaturesToRender];
+
+    console.log('rendering ', this.props.creaturesToRender, creatures)
+    return (
+      <div className="CreaturesContainer">
+        <CreatureListHeader displayType={displayType} displayHemisphere={displayHemisphere} />
+
+        <CreatureList creatures={creatures} />
+      </div>
+    );
+  }
 }
 
-export default CreaturesContainer;
+const mapStateToProps = state => {
+  return {
+    allCreatures: state.creatures.all,
+    currentCreatures: state.creatures.current
+  }
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+    sortByName: () => dispatch(sortByName())
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(CreaturesContainer);
