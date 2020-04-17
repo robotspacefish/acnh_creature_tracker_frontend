e76f43dfd7391cec0f2a5c459c1337a764009b8c
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2020-04-16 22:30:42 -0400

    add creature button

---
+++ src/Creatures/AddCreatureButton.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const AddCreatureButton = props => {
+  const handleOnClick = e => {
+    // todo
+  }
+
+  return (
+    <button onClick={handleOnClick}>Toggle Ownership</button>
+  );
+}
+
+
+export default AddCreatureButton;
