4968f1ef13976a2d54802df15d1cb79931d9493c
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2020-04-16 21:21:22 -0400

    capitalize creature names

--- src/Creatures/Creature.js
+++ src/Creatures/Creature.js
@@ -2,9 +2,14 @@ import React from 'react';
 
 const Creature = ({ creature }) => {
   const { name, c_type, location, shadow_size, price } = creature;
+
+  const capitalize = str => (
+    str.split(" ").map(word => word[0].toUpperCase() + word.slice(1)).join(" ")
+  );
+
   return (
     <tr className="Creature">
-      <th>{name}</th>
+      <th>{capitalize(name)}</th>
       <th>{c_type}</th>
       <th>{location}</th>
       <th>{shadow_size ? shadow_size : "NA"}</th>
