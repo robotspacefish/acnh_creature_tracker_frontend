275174ac93277ce5d8a784ab0dd0a327b06d2d91
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2020-04-17 11:51:16 -0400

    display circle or checked circle depending on ownership

--- src/Creatures/AddCreatureButton.js
+++ src/Creatures/AddCreatureButton.js
@@ -1,14 +1,23 @@
 import React from 'react';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCircle, faCheckCircle } from '@fortawesome/free-solid-svg-icons'
+
+// import { connect } from 'react-redux';
+
 const AddCreatureButton = props => {
+  const { id, isOwned } = props;
   const handleOnClick = e => {
-    // todo
+    // todo pass id to addCreature action to fetch post to user and add creature
+
   }
 
   return (
-    <button onClick={handleOnClick} data-id={props.id}>Toggle Ownership</button>
+    <button onClick={handleOnClick} data-id={id}>
+      <FontAwesomeIcon icon={isOwned ? faCheckCircle : faCircle} />
+    </button>
   );
 }
 
-
 export default AddCreatureButton;
+// export default connect()(AddCreatureButton);
