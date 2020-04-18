0aa34439eb9df814ddb822932616f7ff1f348138
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2020-04-18 16:35:09 -0400

    change to stateless component & handle hemisphere changes

--- src/Creatures/CreaturesHemisphereSelect.js
+++ src/Creatures/CreaturesHemisphereSelect.js
@@ -1,15 +1,19 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-export default class CreaturesHemisphereSelect extends Component {
-  render() {
-    return (
-      <div className="CreaturesHemisphereSelect">
-        <button>Northern Hemisphere</button>
-        <button>Southern Hemisphere</button>
-      </div>
-    );
-  }
+const CreaturesHemisphereSelect = props => {
+  const setActiveClass = type => (props.displayHemisphere === type ? 'active' : '');
+
+  const handleOnClick = e => (
+    props.setHemisphereType(e.target.dataset.hemisphere)
+  );
+
+  return (
+    <div className="CreaturesHemisphereSelect">
+      <button className={`hemisphere-btn ${setActiveClass("north")}`} data-hemisphere="north" onClick={handleOnClick}>Northern Hemisphere</button>
+      <button className={`hemisphere-btn ${setActiveClass("south")}`} data-hemisphere="south" onClick={handleOnClick}>Southern Hemisphere</button>
+    </div>
+  );
+
 }
 
-
-
+export default CreaturesHemisphereSelect;
