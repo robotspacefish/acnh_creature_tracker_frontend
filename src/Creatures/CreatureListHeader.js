8bb10df479190f68be0ba255ac74a10dd0efbd66
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2020-04-18 17:51:57 -0400

    render hemispheres based on current page

--- src/Creatures/CreatureListHeader.js
+++ src/Creatures/CreatureListHeader.js
@@ -3,10 +3,26 @@ import CreaturesTypeSelect from './CreaturesTypeSelect';
 import CreaturesHemisphereSelect from './CreaturesHemisphereSelect';
 
 const CreatureListHeader = props => {
+  const renderHemispheres = () => {
+    const { currentPage, currentUser, currentUserHemisphere, displayHemisphere, setHemisphereType } = props;
+    if (currentUser && currentPage == "user") {
+      const hemisphere = currentUserHemisphere === "north" ? "NORTHERN" : "SOUTHERN";
+
+      return <span className="hemisphere user-hemisphere">{hemisphere} HEMISPHERE</span>
+    } else {
+      return <CreaturesHemisphereSelect
+        displayHemisphere={displayHemisphere}
+        setHemisphereType={setHemisphereType}
+      />
+    }
+  }
+
   return (
     <div className="CreatureListHeader">
       <CreaturesTypeSelect displayType={props.displayType} setDisplayType={props.setDisplayType} />
-      <CreaturesHemisphereSelect displayHemisphere={props.displayHemisphere} setHemisphereType={props.setHemisphereType} />
+
+      {renderHemispheres()}
+
     </div>
   );
 }
