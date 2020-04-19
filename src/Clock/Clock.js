1fbcae0ad36c16cfc91edbb25ce52e7c8212ddf1
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2020-04-19 15:16:54 -0400

    display static time

--- src/Clock/Clock.js
+++ src/Clock/Clock.js
@@ -1,45 +1,8 @@
 import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { fetchCurrentCreatures } from '../actions/creatureActions';
 
 class Clock extends Component {
-  constructor() {
-    super();
-    const date = new Date();
-    this.state = {
-      date: date,
-      startingHour: date.getHours(),
-      startingMinute: date.getMinutes() // for testing
-    };
-  }
-  componentDidMount() {
-    this.timerID = setInterval(
-      () => this.tick(),
-      1000
-    );
-  }
-
-  componentWillUnmount() {
-    clearInterval(this.timerID);
-  }
-
-  tick() {
-    this.setState({
-      date: new Date()
-    }, () => (this.checkForHourChange()))
-  }
-
-  checkForHourChange() {
-    const currentHour = this.state.date.getMinutes() // todo change to hour when done testing
-    if (currentHour !== this.state.startingMinute) {
-      this.props.fetchCurrentCreatures()
-      console.log('updating current creatures')
-      this.setState({
-        // startingHour: this.state.date.getHours(),
-        startingMinute: this.state.date.getMinutes()
-
-      })
-    }
+  state = {
+    date: new Date()
   }
 
   renderDateTime() {
@@ -53,17 +16,9 @@ class Clock extends Component {
     );
   }
 
-
-
   render() {
     return this.renderDateTime();
   }
 }
 
-const mapDispatchToProps = dispatch => {
-  return {
-    fetchCurrentCreatures: () => dispatch(fetchCurrentCreatures())
-  }
-};
-
-export default connect(null, mapDispatchToProps)(Clock);
+export default Clock;
