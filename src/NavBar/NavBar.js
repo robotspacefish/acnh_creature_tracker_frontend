eab95d3c4a27e32621e231d3fe2b426d8d508166
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2020-04-23 12:15:19 -0400

    combine import

--- src/NavBar/NavBar.js
+++ src/NavBar/NavBar.js
@@ -1,10 +1,9 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
+import { NavLink, withRouter } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { logout } from '../actions/userActions';
 import { setPage } from '../actions/appActions';
 import './NavBar.css';
-import { withRouter } from 'react-router-dom'
 
 const NavBar = props => {
   const handleOnClick = e => (props.setPage(e.target.dataset.page));
