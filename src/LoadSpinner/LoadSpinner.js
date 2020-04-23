4d0fadbb72a9fad49b7adb597b48e8cb5aedd7a2
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2020-04-22 23:42:17 -0400

    cleanup

--- src/LoadSpinner/LoadSpinner.js
+++ src/LoadSpinner/LoadSpinner.js
@@ -5,7 +5,6 @@ const LoadSpinner = () => (
   <div className="loadingio-spinner-bean-eater-15tuggfsgar"><div className="ldio-08t6zvnk8ycl">
     <div><div></div><div></div><div></div></div><div><div></div><div></div><div></div></div>
   </div></div>
-
 );
 
 export default LoadSpinner;
