669e4ea8036799b72b8bf6d93b4857fa667cbd89
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2020-04-29 23:36:42 -0400

    use current user to display chosen hemisphere

--- src/Creatures/CreaturesHemisphereSelect.js
+++ src/Creatures/CreaturesHemisphereSelect.js
@@ -4,14 +4,30 @@ const CreaturesHemisphereSelect = props => {
   const setActiveClass = type => (props.displayHemisphere === type ? 'active' : '');
 
   const handleOnClick = e => (
-    // props.setHemisphereType(e.target.dataset.hemisphere)
     props.updateHemisphereType('displayHemisphere', e.target.dataset.hemisphere)
   );
 
+  const renderHemisphereSelect = () => {
+    /** if this is a user's page, only show the hemisphere they selected when signing up, if it's the current creature page, show buttons to select between hemispheres */
+    if (props.isUsersPage()) {
+      const hemisphere = props.currentUserHemisphere === 'north' ? "NORTHERN" : "SOUTHERN";
+
+      return (
+        <span className="hemisphere user-hemisphere">{hemisphere} HEMISPHERE</span>
+      );
+    } else {
+      return (
+        <>
+          <button className={`hemisphere ${setActiveClass("north")}`} data-hemisphere="north" onClick={handleOnClick}>NORTHERN HEMISPHERE</button>
+          <button className={`hemisphere ${setActiveClass("south")}`} data-hemisphere="south" onClick={handleOnClick}>SOUTHERN HEMISPHERE</button>
+        </>
+      );
+    }
+  }
+
   return (
     <div className="CreaturesHemisphereSelect">
-      <button className={`hemisphere ${setActiveClass("north")}`} data-hemisphere="north" onClick={handleOnClick}>NORTHERN HEMISPHERE</button>
-      <button className={`hemisphere ${setActiveClass("south")}`} data-hemisphere="south" onClick={handleOnClick}>SOUTHERN HEMISPHERE</button>
+      {renderHemisphereSelect()}
     </div>
   );
 
