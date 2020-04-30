029be72925393a2b650d9eea4e8f2b8c577caf6b
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2020-04-30 11:02:14 -0400

    adjust ternary to please a warning message
    it didn't like condition && value and wanted condition ? value : undefined to set a className

--- src/Main/Main.js
+++ src/Main/Main.js
@@ -5,7 +5,7 @@ import CreaturesContainer from '../Creatures/CreaturesContainer';
 const Main = props => {
 
   return (
-    <main className={props.match.url.includes('/creatures') && 'scrollbar'}>
+    <main className={props.match.url.includes('/creatures') ? 'scrollbar' : ''}>
       {props.match.url === '/' && <Clock now={props.now} />}
       <CreaturesContainer path={props.match.url} />
     </main>
