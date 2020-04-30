9b4a5e5b4591f4db99aafc77ac4020b4e5cd7fd3
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2020-04-29 20:59:37 -0400

    refactor rendering hemisphere buttons

--- src/Creatures/CreatureListHeader.js
+++ src/Creatures/CreatureListHeader.js
@@ -3,29 +3,18 @@ import CreaturesTypeSelect from './CreaturesTypeSelect';
 import CreaturesHemisphereSelect from './CreaturesHemisphereSelect';
 
 const CreatureListHeader = props => {
-  const renderHemispheres = () => {
-    // TODO get current page from match.url or pass path down
-    const { currentUser, currentUserHemisphere, displayHemisphere, setHemisphereType, updateType } = props;
-    // if (currentUser && currentPage === "user") {
-    if (currentUser === "user") {
-
-      const hemisphere = currentUserHemisphere === "north" ? "NORTHERN" : "SOUTHERN";
-
-      return <span className="hemisphere user-hemisphere">{hemisphere} HEMISPHERE</span>
-    } else {
-      return <CreaturesHemisphereSelect
-        displayHemisphere={displayHemisphere}
-        setHemisphereType={setHemisphereType}
-        updateType={updateType}
-      />
-    }
-  }
 
   return (
     <div className="CreatureListHeader">
-      <CreaturesTypeSelect displayType={props.displayType} setDisplayType={props.setDisplayType} />
+      <CreaturesTypeSelect
+        displayType={props.displayType}
+        updateType={props.updateType}
+      />
 
-      {renderHemispheres()}
+      <CreaturesHemisphereSelect
+        displayHemisphere={props.displayHemisphere}
+        updateType={props.updateType}
+      />
 
     </div>
   );
