f15afffdb57df9d4497b73509dcaa1f8c9022694
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2020-04-30 11:17:45 -0400

    move functions to determine creature availability into helper file

--- src/actions/creatureActions.js
+++ src/actions/creatureActions.js
@@ -1,5 +1,5 @@
 import { BASE_URL } from '../helpers/helpers';
-const moment = require('moment');
+import { isOutAtThisTime, isOutInThisMonth } from '../helpers/creatureHelpers';
 
 export const fetchAllCreatures = () => {
   return dispatch => {
@@ -24,30 +24,4 @@ export const getCurrentlyAvailableCreatures = (creatures, months, hemisphere, no
 
   return { type: "GET_CURRENT_CREATURES", currentCreatures }
 };
-
-const isOutInThisMonth = (creature, months, hem, now) => {
-  const hemisphere = hem === "north" ? 0 : 1;
-  const month = months[now.month()].toLowerCase();
-  return creature.hemispheres[hemisphere][month]
-}
-
-const isOutAtThisTime = (availableTimes, now) => {
-  const startTime = availableTimes.start_time,
-    endTime = availableTimes.end_time;
-
-  if (availableTimes.time === "All day") return true;
-
-  let s = moment();
-  let e = moment();
-  s.hour(startTime)
-  e.hour(endTime)
-
-  if (endTime < startTime) e.day(e.day() + 1)
-
-  // if current time is between start and end times or
-  // if current time is between start time and end time is the next day
-  return (s.hour() <= now.hour() && e.hour() > now.hour()) ||
-    (now.hour() < e.hour() && e.hour() < s.hour() && e.day() > now.day()) ||
-    (s.hour() <= now.hour() && e.day() > now.day());
-}
 
