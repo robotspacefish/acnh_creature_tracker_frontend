dc801d6958d5992ecad20d79ded1c85284e741ae
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2020-04-30 10:49:13 -0400

    scrollbar class
    only show scrollbar on the user's creature page

--- src/Main/Main.js
+++ src/Main/Main.js
@@ -5,10 +5,10 @@ import CreaturesContainer from '../Creatures/CreaturesContainer';
 const Main = props => {
 
   return (
-    <>
+    <main className={props.match.url.includes('/creatures') && 'scrollbar'}>
       {props.match.url === '/' && <Clock now={props.now} />}
       <CreaturesContainer path={props.match.url} />
-    </>
+    </main>
   )
 };
 
