1a797e7e7a0217262a976cb92ee82ba2c4f46572
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2020-04-30 11:14:43 -0400

    refactor & move fetch directly into creatureActions from adapter

--- src/actions/creatureActions.js
+++ src/actions/creatureActions.js
@@ -1,28 +1,20 @@
-import CreatureAdapter from '../adapters/creatureAdapter';
+import { BASE_URL } from '../helpers/helpers';
 const moment = require('moment');
-
-export const fetchCurrentCreatures = () => {
-  return dispatch => {
-    dispatch({ type: "LOADING_CREATURES" });
-
-    CreatureAdapter.fetchCreatures(dispatch, "current", addCurrentCreatures)
-  };
-}
 
 export const fetchAllCreatures = () => {
   return dispatch => {
     dispatch({ type: "LOADING_CREATURES" });
 
-    CreatureAdapter.fetchCreatures(dispatch, "creatures", addAllCreatures)
+    fetch(`${BASE_URL}/creatures`)
+      .then(res => res.json())
+      .then(data => {
+        dispatch({ type: "ADD_ALL_CREATURES", data })
+      })
+      .catch(error => console.error)
   }
 }
 
-const addCurrentCreatures = data => ({ type: "ADD_CURRENT_CREATURES", data });
-
-const addAllCreatures = data => ({ type: "ADD_ALL_CREATURES", data });
-
 export const getCurrentlyAvailableCreatures = (creatures, months, hemisphere, now) => {
-
   const currentCreatures = creatures.filter(creature => (
     creature.availables.every(at => (
       isOutInThisMonth(creature, months, hemisphere, now) &&
