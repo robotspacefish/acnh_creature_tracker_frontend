6a2da0c5a408c840ff0ef29bb5342d64dcffa915
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2020-05-21 19:46:34 -0400

    build folder

---
+++ build/precache-manifest.98df498ee7cc3f8ab1563ab5c3fc1b44.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18eb2453f4aec51559cf2e45a86da980",
+    "url": "/acnh_creature_tracker_frontend/index.html"
+  },
+  {
+    "revision": "aaf2fa165fb92a2ac728",
+    "url": "/acnh_creature_tracker_frontend/static/css/main.91ff3638.chunk.css"
+  },
+  {
+    "revision": "2694d2915072e0d46fee",
+    "url": "/acnh_creature_tracker_frontend/static/js/2.ca82d14c.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/acnh_creature_tracker_frontend/static/js/2.ca82d14c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aaf2fa165fb92a2ac728",
+    "url": "/acnh_creature_tracker_frontend/static/js/main.c3e51b00.chunk.js"
+  },
+  {
+    "revision": "68d31bc9b6c2ffd745b1",
+    "url": "/acnh_creature_tracker_frontend/static/js/runtime-main.aeb5ac06.js"
+  },
+  {
+    "revision": "796b643a146db4b34af5f263f2b19e4a",
+    "url": "/acnh_creature_tracker_frontend/static/media/FinkHeavy.796b643a.ttf"
+  }
+]);
