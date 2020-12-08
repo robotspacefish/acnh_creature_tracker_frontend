3766a202f503fe470a049b50c5479b152af8b7d4
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2020-12-08 18:48:14 -0500

    rename startTime/endTime to startHour/endHour and cleanup

--- src/helpers/creatureHelpers.js
+++ src/helpers/creatureHelpers.js
@@ -12,19 +12,19 @@ export const isOutInThisMonth = (creature, months, hem, now) => {
 }
 
 export const isOutAtThisTime = (availableTimes, now) => {
-  const startTime = availableTimes.start_time,
-    endTime = availableTimes.end_time;
-
   if (availableTimes.time === "All day") return true;
 
-  const endDay = endTime < startTime ? now.day() + 1 : now.day();
-  const nowDay = now.day(), nowHour = now.hour();
+  const startHour = availableTimes.start_time,
+    endHour = availableTimes.end_time,
+    nowDay = now.day(),
+    nowHour = now.hour(),
+    endDay = endHour < startHour ? now.day() + 1 : now.day();
 
   /**
    * if current time is between start and end times or
    * if current time is between start time and end time is the next day
    */
-  return (startTime <= nowHour && endTime > nowHour) ||
-    (nowHour < endTime && endTime < startTime && endDay > nowDay) ||
-    (startTime <= nowHour && endDay > nowDay);
+  return (startHour <= nowHour && endHour > nowHour) ||
+    (nowHour < endHour && endHour < startHour && endDay > nowDay) ||
+    (startHour <= nowHour && endDay > nowDay);
 }
