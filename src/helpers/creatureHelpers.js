0761058adebba0b149bdbbdce5059ab65ceefc15
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2020-12-08 18:37:19 -0500

    check if creature is currently available without using moment or dayjs

--- src/helpers/creatureHelpers.js
+++ src/helpers/creatureHelpers.js
@@ -1,10 +1,8 @@
-const moment = require('moment');
-
 export const isOutInThisMonth = (creature, months, hem, now) => {
   const hemisphere = hem === "north" ? 0 : 1;
 
   const currentMonthIndex = now.month() - 1;
-
+  // debugger
   // failsafe for January until I have a second to look into moment some more
   if (currentMonthIndex === -1) currentMonthIndex = 0;
 
@@ -19,18 +17,14 @@ export const isOutAtThisTime = (availableTimes, now) => {
 
   if (availableTimes.time === "All day") return true;
 
-  let s = moment();
-  let e = moment();
-  s.hour(startTime)
-  e.hour(endTime)
-
-  if (endTime < startTime) e.day(e.day() + 1)
+  const endDay = endTime < startTime ? now.day() + 1 : now.day();
+  const nowDay = now.day(), nowHour = now.hour();
 
   /**
    * if current time is between start and end times or
    * if current time is between start time and end time is the next day
    */
-  return (s.hour() <= now.hour() && e.hour() > now.hour()) ||
-    (now.hour() < e.hour() && e.hour() < s.hour() && e.day() > now.day()) ||
-    (s.hour() <= now.hour() && e.day() > now.day());
+  return (startTime <= nowHour && endTime > nowHour) ||
+    (nowHour < endTime && endTime < startTime && endDay > nowDay) ||
+    (startTime <= nowHour && endDay > nowDay);
 }
