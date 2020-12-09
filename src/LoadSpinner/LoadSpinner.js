795e329dcfe854884e43c8e15b0499c7625fe1b5
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2020-12-08 22:15:29 -0500

    add flex div to Loadspinner to center it & Loading text

--- src/LoadSpinner/LoadSpinner.js
+++ src/LoadSpinner/LoadSpinner.js
@@ -2,9 +2,12 @@ import React from 'react';
 import './LoadSpinner.css';
 
 const LoadSpinner = () => (
-  <div className="loadingio-spinner-bean-eater-15tuggfsgar"><div className="ldio-08t6zvnk8ycl">
-    <div><div></div><div></div><div></div></div><div><div></div><div></div><div></div></div>
-  </div></div>
+  <div className="LoadSpinner">
+    <div className="loadingio-spinner-bean-eater-15tuggfsgar"><div className="ldio-08t6zvnk8ycl">
+      <div><div></div><div></div><div></div></div><div><div></div><div></div><div></div></div>
+    </div></div>
+    <h1>Loading Creatures...</h1>
+  </div>
 );
 
 export default LoadSpinner;
