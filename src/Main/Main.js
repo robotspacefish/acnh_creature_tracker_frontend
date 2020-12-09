ce0f6e2d6225f5206e935415988ae419d68fd67b
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2020-12-08 22:15:38 -0500

    change main to div

--- src/Main/Main.js
+++ src/Main/Main.js
@@ -3,12 +3,11 @@ import Clock from '../Clock/Clock';
 import CreaturesContainer from '../Creatures/CreaturesContainer';
 
 const Main = props => {
-
   return (
-    <main className={props.match.url.includes('/creatures') ? 'scrollbar' : ''}>
+    <div className={props.match.url.includes('/creatures') ? 'scrollbar' : ''}>
       {props.match.url === '/' && <Clock now={props.now} />}
       <CreaturesContainer path={props.match.url} />
-    </main>
+    </div>
   )
 };
 
